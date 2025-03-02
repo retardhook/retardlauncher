19ddf6b8bea6b2706983d4f5b49ce07dcad5f812
zeroo <153208404+zerootoad@users.noreply.github.com>
2025-03-02 21:46:22 +0100

    Add files via upload

---
+++ mojang/api/api.h
@@ -0,0 +1,61 @@
+#pragma once
+#include "utils/utils.h"
+#include <cpr/cpr.h>
+#include <filesystem>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <string>
+#include <vector>
+using json = nlohmann::json;
+
+namespace mojang::api {
+
+struct Rule {
+  std::string action;
+  json features;
+  json os;
+  std::string arch;
+};
+
+struct ArgumentValue {
+  std::vector<std::string> values;
+};
+
+struct Argument {
+  std::vector<Rule> rules;
+  ArgumentValue value;
+};
+
+struct Library {
+  std::string name;
+  std::string url;
+  std::string path;
+
+  std::vector<Rule> rules;
+};
+
+struct JarInfo {
+  std::string version;
+  std::string jsonurl;
+  std::string url;
+  std::string type;
+  std::string release_time;
+  std::string path;
+  std::string mainclass;
+  std::string mlv; // minimum launcher version
+
+  std::vector<Argument> arguments;
+  std::vector<Library> libraries;
+};
+
+class Downloader {
+public:
+  JarInfo get(const std::string &version /*, std::string loader*/,
+              bool should_download);
+
+private:
+  std::vector<Library> parseLibraries(const json &libraries);
+  std::vector<Argument> parseArguments(const json &arguments);
+};
+
+} // namespace mojang::api
