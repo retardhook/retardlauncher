264289c2d53bf8910b5d7d84079218b645871fbc
nniirrii <nyrian@proton.me>
2025-03-03 20:55:12 +0200

    Update launch.h

--- include/mojang/game/launch.h
+++ include/mojang/game/launch.h
@@ -1,14 +1,17 @@
 #pragma once
-#include <mojang/api/api.h>
+#include <mojang/api/download.h>
 #include <mojang/auth/offline.h>
 #include <string>
 
 namespace mojang::game {
 
-class Launcher {
-public:
-  bool launch(const mojang::auth::UserInfo &userInfo,
-              const mojang::api::JarInfo &jarInfo,
-              const std::string &javaPath = "java");
-};
+    class Launcher {
+    public:
+        bool launch(const mojang::auth::UserInfo& userInfo,
+            const mojang::api::JarInfo& jarInfo,
+            const std::string& javaPath = "java");
+
+    private:
+        std::string get_game_dir();
+    };
 } // namespace mojang::game
