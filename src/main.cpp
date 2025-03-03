f40fb7afdcf3ac50efece312bb89eb90303ed06d
zeroo <153208404+zerootoad@users.noreply.github.com>
2025-03-03 02:01:46 +0100

    main.src upd

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <mojang/api/api.h>
+#include <mojang/auth/offline.h>
+#include <mojang/game/launch.h>
 
 int main() {
   mojang::api::Downloader downloader;
+  mojang::game::Launcher launcher;
+  mojang::auth::Offline offline;
 
   auto data = downloader.get("1.21.2", true);
 
@@ -13,6 +17,9 @@ int main() {
   std::cout << "Client json: " << data.jsonurl << std::endl;
   std::cout << "Jar path: " << data.path << std::endl;
   std::cout << "Version release date: " << data.release_time << std::endl;
+
+  auto user = offline.Login("zerootoad");
+  launcher.launch(user, data, "java");
 
   // for (auto i : data.libraries) {
   //   std::cout << "Library name: " << i.name << std::endl;
